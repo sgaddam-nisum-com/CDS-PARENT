9eb15a006ad9b4c6f5e71d28bf0ea6940d529773
Ramesh Polishetti <ramesh.polishetti@macys.com>
2015-05-08 12:57:59 +0530

    RP : Added footer wrapper.

--- cds-web/public/js/app.js
+++
@@ -1,7 +0,0 @@
-/*global define*/
-'use strict';
-
-define(['angular','uiRouter','uiRouterStyles','angularRoute','services/serviceLoader','services/appUrlService','services/cdsService','directives/directiveLoader','controllers/controllerLoader','angularAnimate','angularResource','ngDialog',"ngStorage"], function (angular) {
-    var app = angular.module('CDSHOME', ['ngRoute','serviceModule','directiveModule',"controllerModule",'ngAnimate','ngResource','ui.router','uiRouterStyles','ngDialog','ngStorage']);
-    return app;
-});
