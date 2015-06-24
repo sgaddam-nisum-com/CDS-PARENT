ad6c9e5f51beed1f90e9f2463d7d36880bbf9326
Venkata Saikiran Kokku <vkokku@nisum.com>
2015-06-24 11:30:51 +0530

    VK: CDS: cadre fields based on role

--- cds-web/public/js/controllers/cadreController.js
+++ cds-web/public/js/controllers/cadreController.js
@@ -1,135 +1,145 @@
-define(['controllers/controllerModule','formValidation','validators/cadreValidators','errorMessages/cadreErrors','jquery',"messageHandler"], 
-	function (controllerModule,formValidation,validationMap,errorJson,$,messageHandler) {
+define(['controllers/controllerModule', 'formValidation', 'validators/cadreValidators', 'errorMessages/cadreErrors', 'jquery', "messageHandler"],
+    function(controllerModule, formValidation, validationMap, errorJson, $, messageHandler) {
 
-	controllerModule.controller('cadreController', ['$state','$http',"appUrlService",'$scope','registerService',"cdsService","$sessionStorage", 
-
-
-		function($state,$http,appUrls,$scope,registerService,cdsService, $sessionStorage){
-		
+        controllerModule.controller('cadreController', ['$state', '$http', "appUrlService", '$scope', 'registerService', "cdsService", "$sessionStorage",
 
 
-		 var cdsSession = $sessionStorage.cds = $sessionStorage.cds || {};
-		var self = this,
-		   dataJson={};
-        
-		self.user = {};
-		self.user.cadre={};
-		self.isNotValidForm = false;
+            function($state, $http, appUrls, $scope, registerService, cdsService, $sessionStorage) {
 
 
-        handleGetCadre(cdsSession.currentUserId);
-        
-		var config = {
-            initiate :true,
-            blurValidation: false,
-            htmlValidation : false,
-            submitValidForm : false,
-            runCallBack : false,
-        };
-		var formStack = formValidation.init("#cadreRegistrationForm", validationMap, errorJson, config);
-		
-		registerService.getPartyPositionsOptions(function(resp){				
-			$scope.partyPositionsOptions = resp.data;			
-		});
-		registerService.getPerformanceGradeOptions(function(resp){				
-			$scope.performanceGradeOptions = resp.data;			
-		});
 
-		self.user = {};
+                var
+                    cdsSession = $sessionStorage.cds = $sessionStorage.cds || {},
+                    config,
+                    dataJson = {},
+                    formStack,
+                    self = this;
 
-		this.backview = function(e){
-			e.preventDefault();
-			$state.go("root.profile.editprofile.family");
-		}
-				
-		this.save = function(){
-			self.user.userId = cdsService.getUserId();
-			if(formStack.isValid){	
-				self.user.userId = cdsSession.currentUserId;							
-				$http({
-					method: dataJson.reqMethod,
-					url: dataJson.reqURL,
-					data: self.user	
-				}).success(function(resp, status, headers, config){	
-				    if(resp.status ==="success"){			
-					if(self.user.userId){
-						messageHandler.showInfoStatus(errorJson.successfulSave,".status-message-wrapper");
-                        setTimeout(function(){
-                            messageHandler.clearMessageStatus();                           
-                           $state.go("root.profileLookup", {citizenId : self.user.userId});  
-                        },2000); 
-					}else{
-						messageHandler.showInfoStatus(errorJson.successfulSave,".status-message-wrapper");
-                        setTimeout(function(){
-                            messageHandler.clearMessageStatus();                           
-                          $state.go('root.profile');  
-                        },2000); 						
-					}}else{						
-						  messageHandler.showErrorStatus(resp.data.message,".status-message-wrapper");
-	                         setTimeout(function(){
-                            messageHandler.clearMessageStatus();                           
-                        },2000); 						
+                self.user = {};
+                self.user.cadre = {};
+                self.isNotValidForm = false;
 
-					}							
+                handleGetCadre(cdsSession.currentUserId);
 
-				}).error(function(data, status, headers, config){
-					  messageHandler.showErrorStatus(errorJson.submissionError,".status-message-wrapper");
-                        setTimeout(function(){
-                            messageHandler.clearMessageStatus();                           
-                        },2000); 						
+                config = {
+                    initiate: true,
+                    blurValidation: false,
+                    htmlValidation: false,
+                    submitValidForm: false,
+                    runCallBack: false,
+                };
+                formStack = formValidation.init("#cadreRegistrationForm", validationMap, errorJson, config);
 
-				});
+                registerService.getPartyPositionsOptions(function(resp) {
+                    $scope.partyPositionsOptions = resp.data;
+                });
+                registerService.getPerformanceGradeOptions(function(resp) {
+                    $scope.performanceGradeOptions = resp.data;
+                });
 
-				self.isNotValidForm = false;
-			}else{
-				self.isNotValidForm = true;
+                self.user = {};
 
-			} 
-		}
+                this.backview = function(e) {
+                    e.preventDefault();
+                    $state.go("root.profile.editprofile.family");
+                }
 
+                this.save = function() {
+                    self.user.userId = cdsService.getUserId();
+                    if (formStack.isValid) {
+                        self.user.userId = cdsSession.currentUserId;
+                        $http({
+                            method: dataJson.reqMethod,
+                            url: dataJson.reqURL,
+                            data: self.user
+                        }).success(function(resp, status, headers, config) {
+                            if (resp.status === "success") {
+                                if (self.user.userId) {
+                                    messageHandler.showInfoStatus(errorJson.successfulSave, ".status-message-wrapper");
+                                    setTimeout(function() {
+                                        messageHandler.clearMessageStatus();
+                                        $state.go("root.profileLookup", {
+                                            citizenId: self.user.userId
+                                        });
+                                    }, 2000);
+                                } else {
+                                    messageHandler.showInfoStatus(errorJson.successfulSave, ".status-message-wrapper");
+                                    setTimeout(function() {
+                                        messageHandler.clearMessageStatus();
+                                        $state.go('root.profile');
+                                    }, 2000);
+                                }
+                            } else {
+                                messageHandler.showErrorStatus(resp.data.message, ".status-message-wrapper");
+                                setTimeout(function() {
+                                    messageHandler.clearMessageStatus();
+                                }, 2000);
 
-		  function handleGetCadre(userId) {
-            
-                registerService.getCadreInfo( userId,function(resp) {                  
-                    dataJson= resp.data;
+                            }
 
-                    if(dataJson.cadreId){
-                    	dataJson.reqMethod = "PUT";
-                    	dataJson.reqURL =appUrls.updateCadre; 
-                    }else{
-                    	 dataJson.reqMethod = "POST";
-        				 dataJson.reqURL = appUrls.saveCadre;		
+                        }).error(function(data, status, headers, config) {
+                            messageHandler.showErrorStatus(errorJson.submissionError, ".status-message-wrapper");
+                            setTimeout(function() {
+                                messageHandler.clearMessageStatus();
+                            }, 2000);
+
+                        });
+
+                        self.isNotValidForm = false;
+                    } else {
+                        self.isNotValidForm = true;
+
                     }
-               
-                self.user.cadre = {};    
-	        	self.user.interestedAsCadre = "1";  
+                }
 
-	        	if(dataJson.citizen){  
-	            self.user.healthInsurance = dataJson.citizen.healthInsurance;
-	            self.user.lifeInsurance = dataJson.citizen.lifeInsurance;
-	            self.user.bloodGroupId =dataJson.citizen.bloodGroup.bloodGroupId;	            
-	        	}
 
-	            self.user.cadre.positionId =dataJson.partyDesigination.positionId || "";
-	            self.user.cadre.partyMembershipId =dataJson.partyMembershipId;
-	            self.user.cadre.partyResponsibility =dataJson.partyResponsibility;
-				self.user.cadre.performanceGradeId =dataJson.performanceGradeId;
+                function handleGetCadre(userId) {
 
-				 });
+                    registerService.getCadreInfo(userId, function(resp) {
+                        dataJson = resp.data;
+
+                        if (dataJson.cadreId) {
+                            dataJson.reqMethod = "PUT";
+                            dataJson.reqURL = appUrls.updateCadre;
+                        } else {
+                            dataJson.reqMethod = "POST";
+                            dataJson.reqURL = appUrls.saveCadre;
+                        }
+                        cdsService.getUserSession(function(resp) {
+                            console.log(resp.data);
+                            role = resp.data.user.appRoles;
+                            console.log(role);
+                            if (role[role.length - 1].roleName == "Citizen" || role[role.length - 1].roleName == "Volunteer" || role[role.length - 1].roleName == "Cadre") {
+
+                                self.disableFields = true;
+                                self.showGrade = false;
+                            }
+
+                            if (role[role.length - 1].roleName == "Office Manager" || role[role.length - 1].roleName == "Office Executive" || role[role.length - 1].roleName == "MP") {
+
+                                self.disableFields = false;
+                                self.showGrade = true;
+                            }
+
+                        });
+
+                        self.user.cadre = {};
+                        self.user.interestedAsCadre = "1";
+
+                        if (dataJson.citizen) {
+                            self.user.healthInsurance = dataJson.citizen.healthInsurance;
+                            self.user.lifeInsurance = dataJson.citizen.lifeInsurance;
+                            self.user.bloodGroupId = dataJson.citizen.bloodGroup.bloodGroupId;
+                        }
+
+                        self.user.cadre.positionId = dataJson.partyDesigination.positionId || "";
+                        self.user.cadre.partyMembershipId = dataJson.partyMembershipId;
+                        self.user.cadre.partyResponsibility = dataJson.partyResponsibility;
+                        self.user.cadre.performanceGradeId = dataJson.performanceGradeId;
+
+                    });
+                }
             }
+        ]);
 
-             function handleUserEdit() {
-
-
-
-
-
-             }
-         
-
-
-
-	}]);
-
-});
-
+    });
--- cds-web/public/js/controllers/familyController.js
+++ cds-web/public/js/controllers/familyController.js
@@ -1,148 +1,144 @@
-define(['controllers/controllerModule', 'formValidation', 'validators/familyValidators', 'errorMessages/familyErrors', 'jquery',"messageHandler"], 
+define(['controllers/controllerModule', 'formValidation', 'validators/familyValidators', 'errorMessages/familyErrors', 'jquery', "messageHandler"],
 
-	function(controllerModule, formValidation, validationMap, errorJson, $,messageHandler) {
+    function(controllerModule, formValidation, validationMap, errorJson, $, messageHandler) {
 
+        controllerModule.controller('familyController', ['$state', '$http', "appUrlService", "cdsService", '$scope', 'registerService', "$sessionStorage",
 
-		console.log(errorJson);
+            function($state, $http, appUrls, cdsService, $scope, registerService, $sessionStorage) {
 
+                var self = this,
+                    dataJson = {};
+                self.isNotValidForm = false;
 
-    controllerModule.controller('familyController', ['$state', '$http', "appUrlService", "cdsService", '$scope', 'registerService', "$sessionStorage",
+                var cdsSession = $sessionStorage.cds = $sessionStorage.cds || {};
 
-        function($state, $http, appUrls, cdsService, $scope, registerService, $sessionStorage) {
-
-            var self = this,
-                dataJson = {};
-            self.isNotValidForm = false;
-
-            var cdsSession = $sessionStorage.cds = $sessionStorage.cds || {};
-
-            var reqMethod = "PUT";
-            var reqURL = appUrls.updateFamily;
+                var reqMethod = "PUT";
+                var reqURL = appUrls.updateFamily;
 
 
-            handleGetFamily(cdsSession.currentUserId);
+                handleGetFamily(cdsSession.currentUserId);
 
-            var config = {
-                initiate: true,
-                blurValidation: false,
-                htmlValidation: false,
-                submitValidForm: false,
-                runCallBack: false,
-            };
+                var config = {
+                    initiate: true,
+                    blurValidation: false,
+                    htmlValidation: false,
+                    submitValidForm: false,
+                    runCallBack: false,
+                };
 
-            registerService.getEducationOptions(function(resp) {
-                $scope.educationOptions = resp.data;
-            });
-
-            var formStack = formValidation.init("#familyRegistrationForm", validationMap, errorJson, config);
-
-
-            this.backview = function(e){
-            	e.preventDefault();
-            	$state.go("root.profile.editprofile.volunteer");
-            }
-
-            this.save = function() {
-
-                if (formStack.isValid) {
-
-                    var requestData = [];
-                    var spouseObj = {};
-                    var childObj = {};
-                    spouseObj.relationType = "Wife";
-                    spouseObj.educationId = self.user.spouseData.education.educationId;
-                    spouseObj.firstName = self.user.spouseData.firstName;
-                    spouseObj.lastName = self.user.spouseData.lastName;
-                    spouseObj.middleName = self.user.spouseData.middleName;
-                    spouseObj.gender = self.user.spouseData.gender;
-                    spouseObj.marriageDate = self.user.spouseData.marriageDate;
-                    spouseObj.dateOfBirth = self.user.spouseData.dateOfBirth;
-                    requestData.push(spouseObj);
-
-                    if (self.user.childData.length && self.user.childData[0].firstName) {
-
-                        for (var j = 0; j < self.user.childData.length; j++) {
-                            childObj.relationType = "Kid";
-                            childObj.educationId = self.user.childData[j].education.educationId;
-                            childObj.firstName = self.user.childData[j].firstName;
-                            childObj.middleName = self.user.childData[j].middleName;
-                            childObj.lastName = self.user.childData[j].lastName;
-                            childObj.gender = self.user.childData[j].gender;
-                            childObj.marriageDate = self.user.childData[j].marriageDate;
-                            childObj.dateOfBirth = self.user.childData[j].dateOfBirth;
-                            requestData.push(childObj);
-                        }
-
-                    }
-                    var familyReqObj = {};
-                    familyReqObj.data = requestData;
-                    familyReqObj.userId = cdsSession.currentUserId;
-
-                    $http({
-                        method: reqMethod,
-                        url: reqURL,
-                        data: familyReqObj
-                    }).success(function(resp, status, headers, config) {
-                        if (resp.status == "success") {                        
-                        	messageHandler.showInfoStatus(errorJson.successfulSave,".status-message-wrapper"); 
-                        	setTimeout(function(){
-                            	messageHandler.clearMessageStatus();                           
-                           		$state.go('root.profile.editprofile.cadre');	
-                        	},2000);                        	                        	                        	
-                        
-                        } else {
-                        	messageHandler.showErrorStatus(errorJson.submissionError,".status-message-wrapper");
-                        	setTimeout(function(){
-                        	 messageHandler.clearMessageStatus(); 
-                        	},2000);
-                        }
-
-                    }).error(function(data, status, headers, config) {
-                    	messageHandler.showErrorStatus(errorJson.submissionError,".status-message-wrapper");
-                    		setTimeout(function(){
-                        	 messageHandler.clearMessageStatus(); 
-                        	},2000);
-                    });
-
-                } else {
-                    self.isNotValidForm = true;
-               }
-            }
-
-
-            function handleGetFamily(userId) {
-                registerService.getFamilyInfo(userId, function(resp) {
-
-                    console.log(resp);
-
-                    dataJson = resp.data;
-                    self.user = {};
-                    self.user.childData = [];
-                    /*To show primary field*/
-                    if (dataJson.length < 2 || !dataJson.length) {
-                        self.user.childData = [{}];
-                    }
-
-                    if (resp.status == "success") {
-
-                        self.users = resp.data;
-
-                        for (var i = 0; i < self.users.length; i++) {
-                            if (self.users[i].relationType == "Wife") {
-                                self.user.spouseData = self.users[i];
-                            } else {
-                                self.user.childData.push(self.users[i]);
-                            }
-                        };
-
-
-                    }
-
+                registerService.getEducationOptions(function(resp) {
+                    $scope.educationOptions = resp.data;
                 });
 
+                var formStack = formValidation.init("#familyRegistrationForm", validationMap, errorJson, config);
+
+
+                this.backview = function(e) {
+                    e.preventDefault();
+                    $state.go("root.profile.editprofile.volunteer");
+                }
+
+                this.save = function() {
+
+                    if (formStack.isValid) {
+
+                        var requestData = [];
+                        var spouseObj = {};
+                        var childObj = {};
+                        spouseObj.relationType = "Wife";
+                        spouseObj.educationId = self.user.spouseData.education.educationId;
+                        spouseObj.firstName = self.user.spouseData.firstName;
+                        spouseObj.lastName = self.user.spouseData.lastName;
+                        spouseObj.middleName = self.user.spouseData.middleName;
+                        spouseObj.gender = self.user.spouseData.gender;
+                        spouseObj.marriageDate = self.user.spouseData.marriageDate;
+                        spouseObj.dateOfBirth = self.user.spouseData.dateOfBirth;
+                        requestData.push(spouseObj);
+
+                        if (self.user.childData.length && self.user.childData[0].firstName) {
+
+                            for (var j = 0; j < self.user.childData.length; j++) {
+                                childObj.relationType = "Kid";
+                                childObj.educationId = self.user.childData[j].education.educationId;
+                                childObj.firstName = self.user.childData[j].firstName;
+                                childObj.middleName = self.user.childData[j].middleName;
+                                childObj.lastName = self.user.childData[j].lastName;
+                                childObj.gender = self.user.childData[j].gender;
+                                childObj.marriageDate = self.user.childData[j].marriageDate;
+                                childObj.dateOfBirth = self.user.childData[j].dateOfBirth;
+                                requestData.push(childObj);
+                            }
+
+                        }
+                        var familyReqObj = {};
+                        familyReqObj.data = requestData;
+                        familyReqObj.userId = cdsSession.currentUserId;
+
+                        $http({
+                            method: reqMethod,
+                            url: reqURL,
+                            data: familyReqObj
+                        }).success(function(resp, status, headers, config) {
+                            if (resp.status == "success") {
+                                messageHandler.showInfoStatus(errorJson.successfulSave, ".status-message-wrapper");
+                                setTimeout(function() {
+                                    messageHandler.clearMessageStatus();
+                                    $state.go('root.profile.editprofile.cadre');
+                                }, 2000);
+
+                            } else {
+                                messageHandler.showErrorStatus(errorJson.submissionError, ".status-message-wrapper");
+                                setTimeout(function() {
+                                    messageHandler.clearMessageStatus();
+                                }, 2000);
+                            }
+
+                        }).error(function(data, status, headers, config) {
+                            messageHandler.showErrorStatus(errorJson.submissionError, ".status-message-wrapper");
+                            setTimeout(function() {
+                                messageHandler.clearMessageStatus();
+                            }, 2000);
+                        });
+
+                    } else {
+                        self.isNotValidForm = true;
+                    }
+                }
+
+
+                function handleGetFamily(userId) {
+                    registerService.getFamilyInfo(userId, function(resp) {
+
+
+
+                        dataJson = resp.data;
+                        self.user = {};
+                        self.user.childData = [];
+                        /*To show primary field*/
+                        if (dataJson.length < 2 || !dataJson.length) {
+                            self.user.childData = [{}];
+                        }
+
+                        if (resp.status == "success") {
+
+                            self.users = resp.data;
+
+                            for (var i = 0; i < self.users.length; i++) {
+                                if (self.users[i].relationType == "Wife") {
+                                    self.user.spouseData = self.users[i];
+                                } else {
+                                    self.user.childData.push(self.users[i]);
+                                }
+                            };
+
+
+                        }
+
+                    });
+
+                }
+
             }
+        ]);
 
-        }
-    ]);
-
-});
+    });
